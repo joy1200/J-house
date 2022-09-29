9ef5b98b8c81ed5954bad02fbca2e2da854fa625
joy1200 <tjoeun@DESKTOP-0LMN2KA>
2022-09-29 17:20:20 +0900

    Commit

---
+++ java_0929/src/java_0929/ConsoleDisplay.java
@@ -0,0 +1,25 @@
+package java_0929;
+
+public class ConsoleDisplay {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("World");
+
+		System.out.print("Hello");
+		System.out.println("World");
+		
+		System.out.print("Hello" + "World");
+	
+		System.out.println(10 + 7);
+		System.out.println("World".toUpperCase());
+		
+		System.out.println("3 " + "+ " + "5 " + "= " + (3 + 5) );
+
+		System.out.printf("%5d\n", 20);
+		System.out.printf("%5d\n", 1220);		
+		
+		System.out.printf("%f\n", 1225.12515);
+	}
+
+}
---
+++ java_0929/src/java_0929/Second.java
@@ -0,0 +1,16 @@
+package java_0929;
+
+public class Second {
+
+	public static void main(String[] args) {
+		System.out.println("출력");
+		System.out.println("출력");
+		/* 
+		 * 주석 입니다
+		 * 주석 
+		 * 입니다 입니다 
+		 * 
+		 */
+				     			}
+
+}
