430dd37feade50ee3cd2fab1df156866f64fa821
joy1200 <tjoeun@DESKTOP-0LMN2KA>
2022-09-29 17:46:13 +0900

    Y

---
+++ java_basic/src/java_basic/Console_Display.java
@@ -0,0 +1,26 @@
+package java_basic;
+
+public class Console_Display {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("World");
+
+		System.out.print("Hello");
+		System.out.println("World");
+		
+		System.out.print("Hello" + "World");
+	
+		System.out.println(10 + 7);
+		System.out.println("World".toUpperCase());
+		
+		System.out.println("3 " + "+ " + "5 " + "= " + (3 + 5) );
+
+		System.out.printf("%5d\n", 20);
+		System.out.printf("%5d\n", 1220);		
+		
+		System.out.printf("%f\n", 1225.12515);
+		
+	}
+
+}
--- java_basic/src/java_basic/First.java
+++ java_basic/src/java_basic/First.java
@@ -1,6 +1,6 @@
-package java_0928;
+package java_basic;
 
-public class first {
+public class First {
 
 	public static void main(String[] args) {
 		System.out.println("20220928");
